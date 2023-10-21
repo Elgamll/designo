26730a6ebae2f83a43d6af31e71fb2cec5b5ecb9
Mohamad Elgaml <melgaml@icloud.com>
2023-10-21 22:04:08 +0200

    feat: home page

---
+++ index.js
@@ -0,0 +1,7 @@
+document.querySelector(".menu-icon").addEventListener("click", function () {
+  // Code to execute when the icon is clicked
+});
+
+document.querySelector(".menu-icon").addEventListener("click", function () {
+  document.querySelector(".menu-container").classList.toggle("open");
+});
